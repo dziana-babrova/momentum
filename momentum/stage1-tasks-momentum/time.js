baeb2a954629594872d29903f1903bcc894ccc82
Diana Bobrova <dzianababrova@gmail.com>
2022-08-04 14:58:39 +0300

    feat: add greeting

--- momentum/stage1-tasks-momentum/time.js
+++ momentum/stage1-tasks-momentum/time.js
@@ -12,6 +12,38 @@ function showTime() {
     dateElement.textContent = currentDate;
   }
   showDate();
+  const greeting = document.querySelector(".greeting");
+
+  function getTimeOfDay() {
+    const date = new Date();
+    const hours = date.getHours();
+    if (hours >= 0 && hours < 6) {
+      return "Спокойной ночи,";
+    } else if (hours >= 6 && hours < 12) {
+      return "Доброе утро,";
+    } else if (hours >= 12 && hours < 18) {
+      return "Добрый день,";
+    } else if (hours >= 18 && hours < 24) {
+      return "Добрый вечер,";
+    }
+  }
+
+  const timeOfDay = getTimeOfDay();
+  greeting.textContent = timeOfDay;
 }
 showTime();
+
+const userName = document.querySelector(".name");
+
+function setLocalStorage() {
+  localStorage.setItem("name", userName.value);
+}
+window.addEventListener("beforeunload", setLocalStorage);
+
+function getLocalStorage() {
+  if (localStorage.getItem("name")) {
+    userName.value = localStorage.getItem("name");
+  }
+}
+window.addEventListener("load", getLocalStorage);
 
